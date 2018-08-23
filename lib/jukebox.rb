726635c55d204c21e7834f98c2037f29ae12a528
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-23 13:26:21 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,3 +16,10 @@ def help
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1} - #{song}"
+  end 
+end 
