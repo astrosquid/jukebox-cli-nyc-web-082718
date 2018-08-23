213b10a28623ba0a1c140c561894a27c404b791e
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-23 13:29:47 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,3 +31,6 @@ def play(songs)
     puts "Playing #{input}"
   elsif input.to_i > 0 && input.to_i <= songs.length 
     puts "Playing #{songs[input.to_i]}"
+  else 
+    puts "Invalid input, please try again"
+  end
